33ce0c1b831aff920147394ea39e7e5ddda12399
Prerna <rprerna2000@gmail.com>
2025-03-09 00:24:44 +0530

    fix: toast functionality

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,31 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faCircleCheck,
+  faCircleExclamation,
+  faCircleInfo,
+  faTriangleExclamation,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-toast',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.css',
 })
 export class ToastComponent {
   @Input() message = '';
-  @Input() duration = 3000;
-  @Input() messageType = 'success';
-  visible = false;
+  @Input() duration = 7000;
+  @Input() messageType: 'success' | 'error' | 'warning' | 'info' = 'success';
+  showToast = false;
+  faCircleCheck = faCircleCheck;
+  faTriangleExclamation = faTriangleExclamation;
+  faCircleExclamation = faCircleExclamation;
+  faCircleInfo = faCircleInfo;
 
   ngOnInit() {
-    console.log('called');
-    if (this.message) {
-      this.showToast();
-    }
   }
 
-  showToast() {
-    this.visible = true;
+  showToastPopup(
+    message: string,
+    messageType: 'success' | 'error' | 'warning' | 'info'
+  ) {
+    this.message = message;
+    this.messageType = messageType;
+    this.showToast = true;
     setTimeout(() => {
-      this.visible = false;
+      this.showToast = false;
       this.message = '';
     }, this.duration);
   }
-}
