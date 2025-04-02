cd7589e8975264199103b94c67737ea7911bfc28
Prerna <rprerna2000@gmail.com>
2025-04-02 11:42:34 +0530

    style: organize tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,22 +52,43 @@ module.exports = {
 };
 
 // List of all colors used in entire website
+
+// Tailwind
+// gray-100: "#F3F4F6";
 // gray-200: #E5E5E5;
 // gray-300: #D1D1D1;
-// gray-400: #B3B3B3;
-// gray-500: #A0A0A0;
+// gray-400: #9CA3AF;
+// gray-500: #6B7280;
 // gray-600: #7D7D7D;
-// gray-700: #4B4B4B;
-// gray-800: #2D2D2D;
-// primary-500: #1A365D; (Also bg-primary-500/90)
-// Primary-400: #3B82F6;
+// gray-700: #374151;
+// gray-800: #1F2937;
+// gray-900: "#111827";
+
+// red-50: "#FEF2F2";
+// red-100: "#FEE2E2";
 // red-200: #FCA5A5;
+// red-300 → "#FCA5A5";
+// red-400: "#F87171";
 // red-500: #EF4444;
 // red-600: #DC2626;
+// red-700 → "#B91C1C";
 // red-800: #991B1B;
+// red-900/20 → rgba(127, 29, 29, 0.2) (Equivalent to #7F1D1D with 20% opacity)
+
+// blue-50: "#EFF6FF";
+// blue-100: "#DBEAFE";
+// blue-400: "#60A5FA";
+// blue-500: "#3B82F6";
+// blue-600: "#2563EB";
+// blue-900/20 → rgba(30, 58, 138, 0.2) (Equivalent to #1E3A8A with 20% opacity)
+
+// white: #FFFFFF;
+
+// Custom
+// primary-500: #1A365D; (Also bg-primary-500/90)
+// Primary-400: #3B82F6;
 // background-secondary: #F3F4F6;
 // background-dark-secondary: #1F2937;
-// white: #FFFFFF;
 // background-default: "#F7FAFC";
 // background-dark: "#111827";
 // secondary: "#4A5568";
